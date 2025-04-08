70f3d302c8baad075a7f9d096572a74f1313bb20
Javier Valenzuela <137245545+javierexe@users.noreply.github.com>
2025-04-08 14:55:22 -0400

    Cambio a inglés de nombre de variables para mejorar legibilidad

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
 //write your code here
-let pronoun = ["the", "our"];
-let adj = ["great", "big"];
-let noun = ["jogger", "racoon"];
-let ext = [".com", ".net", ".us", ".io", ".cl"];
+let pronouns = ["the", "our"];
+let adjetives = ["great", "big"];
+let nouns = ["jogger", "racoon"];
+let extensions = [".com", ".net", ".us", ".io", ".cl"];
 
-pronoun.forEach((pronombre) => {
-  adj.forEach((adjetivo) => {
-    noun.forEach((sujeto) => {
-      ext.forEach((extension) => {
-        let dominio = pronombre + adjetivo + sujeto + extension;
-        console.log(dominio);
+pronouns.forEach((pronoun) => {
+  adjetives.forEach((adj) => {
+    nouns.forEach((subj) => {
+      extentions.forEach((ext) => {
+        let domain = pronoun + adj + subj + ext;
+        console.log(domain);
       });
     });
   });
